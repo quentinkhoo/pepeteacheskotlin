8e3f90219bd03728a3c21628b884e3ac4fac27ab
Quentin Khoo <quentinkhoo@outlook.com>
2018-10-08 15:11:27 +0800

    Initial Commit

---
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/BasicTypesPart7Fragment.kt
@@ -0,0 +1,41 @@
+package com.quentin.is3261.pepeteacheskotlin
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class BasicTypesPart7Fragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BasicTypesPart7Fragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val myView = inflater.inflate(R.layout.fragment_basic_types_part7, container, false)
+
+        val finishLesson1Button = myView.findViewById<Button>(R.id.finishlesson1_butt)
+        finishLesson1Button.setOnClickListener {
+            getActivity()?.finish()
+        }
+
+        return myView
+    }
+
+
+}
---
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.quentin.is3261.pepeteacheskotlin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.CardView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val cardviewlesson1 = findViewById<CardView>(R.id.cardviewlesson1)
+        cardviewlesson1.setOnClickListener {
+            val BasicTypesActivity = Intent(this, LessonBasicTypesActivity::class.java)
+            startActivity(BasicTypesActivity)
+        }
+
+        val cardviewlesson2 = findViewById<CardView>(R.id.cardviewlesson2)
+        cardviewlesson2.setOnClickListener {
+            Toast.makeText(this, "In progress", Toast.LENGTH_LONG).show()
+        }
+    }
+}
