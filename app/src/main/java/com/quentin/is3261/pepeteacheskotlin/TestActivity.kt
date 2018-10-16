1efca2118be44b9dd46e4016e8d4f52e26f1de24
DESKTOP-J7EKCCK\SYLVIA <sylxswee@gmail.com>
2018-10-17 02:11:17 +0800

    designed in testactivity for lessons

---
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/TestActivity.kt
@@ -0,0 +1,15 @@
+package com.quentin.is3261.pepeteacheskotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.GridView
+import android.widget.ScrollView
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+    }
+}
