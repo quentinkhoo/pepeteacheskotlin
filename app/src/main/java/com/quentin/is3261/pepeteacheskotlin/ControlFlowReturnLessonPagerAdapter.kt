567037ec278663fe91e358541764eb4796ea0d7d
Quentin Khoo <quentinkhoo@outlook.com>
2018-11-05 04:12:23 +0800

    Add return lesson

---
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/ControlFlowReturnLessonPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.quentin.is3261.pepeteacheskotlin
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class ControlFlowReturnLessonPagerAdapter(fragmentManager: FragmentManager): FragmentPagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return 1
+    }
+
+    override fun getItem(position: Int): Fragment? {
+        return when (position) {
+            0 -> ControlFlowReturnLessonPart1.newInstance()
+
+            else ->
+                null
+        }
+    }
+
+}
