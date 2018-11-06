56b8ab0fe02747b8d11adf6d83072b6d1a791e24
Quentin Khoo <quentinkhoo@outlook.com>
2018-11-07 02:35:54 +0800

    Add spin animation on dashboard click

--- app/src/main/java/com/quentin/is3261/pepeteacheskotlin/DashboardRecyclerAdapter.kt
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/DashboardRecyclerAdapter.kt
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -59,48 +60,51 @@ class DashboardRecyclerAdapter(val context: Context) : RecyclerView.Adapter<Dash
                 var myIntent: Intent? = null
                 override fun onClick(v: View) {
                     val position = getAdapterPosition()
-                    when (position) {
-                        0 -> {
-                            myIntent = Intent(context, BasicTypesNumbersLessonActivity::class.java)
-                            //context.startActivity(myIntent);
-                            (context as Activity).startActivity(myIntent)
-                        }
-                        1 -> {
-                            myIntent = Intent(context, BasicTypesCharactersLessonActivity::class.java)
-                            (context as Activity).startActivity(myIntent)
-                        }
-                        2 -> {
-                            myIntent = Intent(context, BasicTypeBooleanLessonActivity::class.java)
-                            (context as Activity).startActivity(myIntent)
 
-                        }
-                        3 -> {
-                            myIntent = Intent(context, BasicTypesStringLessonActivity::class.java)
-                            (context as Activity).startActivity(myIntent)
+                    v.startAnimation(AnimationUtils.loadAnimation(context, R.anim.spin_rotate))
+                    v.postDelayed({
+                        when (position) {
+                            0 -> {
+                                myIntent = Intent(context, BasicTypesNumbersLessonActivity::class.java)
+                                //context.startActivity(myIntent);
+                                (context as Activity).startActivity(myIntent)
+                            }
+                            1 -> {
+                                myIntent = Intent(context, BasicTypesCharactersLessonActivity::class.java)
+                                (context as Activity).startActivity(myIntent)
+                            }
+                            2 -> {
+                                myIntent = Intent(context, BasicTypeBooleanLessonActivity::class.java)
+                                (context as Activity).startActivity(myIntent)
 
-                        }
-                        4 -> {
-                            myIntent = Intent(context, BasicTypesArraysLessonActivity::class.java)
-                            (context as Activity).startActivity(myIntent)
+                            }
+                            3 -> {
+                                myIntent = Intent(context, BasicTypesStringLessonActivity::class.java)
+                                (context as Activity).startActivity(myIntent)
 
-                        }
-                        5 -> {
-                            myIntent = Intent(context, BasicTypesCollectionsLessonActivity::class.java)
-                            (context as Activity).startActivity(myIntent)
+                            }
+                            4 -> {
+                                myIntent = Intent(context, BasicTypesArraysLessonActivity::class.java)
+                                (context as Activity).startActivity(myIntent)
 
-                        }
-                        6 -> {
-                            myIntent = Intent(context, BasicTypesRangesLessonActivity::class.java)
-                            (context as Activity).startActivity(myIntent)
+                            }
+                            5 -> {
+                                myIntent = Intent(context, BasicTypesCollectionsLessonActivity::class.java)
+                                (context as Activity).startActivity(myIntent)
 
-                        }
-                        7 -> {
-                            myIntent = Intent(context, BasicTypesQuizActivity::class.java)
-                            (context as Activity).startActivityForResult(myIntent, BASIC_QUIZ_REQUEST_CODE)
-                        }
-                        else -> Toast.makeText(context, "Coming Soon!", Toast.LENGTH_SHORT).show()
-                    }
+                            }
+                            6 -> {
+                                myIntent = Intent(context, BasicTypesRangesLessonActivity::class.java)
+                                (context as Activity).startActivity(myIntent)
 
+                            }
+                            7 -> {
+                                myIntent = Intent(context, BasicTypesQuizActivity::class.java)
+                                (context as Activity).startActivityForResult(myIntent, BASIC_QUIZ_REQUEST_CODE)
+                            }
+                            else -> Toast.makeText(context, "Coming Soon!", Toast.LENGTH_SHORT).show()
+                        }
+                    }, 500)
                 }
 
             })
