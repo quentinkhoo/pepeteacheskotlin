cb49181c1152fff47fe8c050bf92692746ae3591
Quentin Khoo <quentinkhoo@outlook.com>
2018-11-08 04:26:33 +0800

    Update class object urls

--- app/src/main/java/com/quentin/is3261/pepeteacheskotlin/ClassObjectInnerClassLessonPart3.kt
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/ClassObjectInnerClassLessonPart3.kt
@@ -1,11 +1,14 @@
 package com.quentin.is3261.pepeteacheskotlin
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_class_object_inner_class_lesson_part1.view.*
+import kotlinx.android.synthetic.main.fragment_class_object_inner_class_lesson_part3.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,7 +22,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class ClassObjectInnerClassLessonPart3 : Fragment() {
 
-    private val url = "https://www.tutorialspoint.com/tpcg.php?p=TdGTLL"
+    private val url = "https://pl.kotl.in/ry8V96gTm"
 
     companion object {
         fun newInstance() = ClassObjectInnerClassLessonPart3()
@@ -28,7 +31,15 @@ class ClassObjectInnerClassLessonPart3 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_class_object_inner_class_lesson_part3, container, false)
+        val myView = inflater.inflate(R.layout.fragment_class_object_inner_class_lesson_part3, container, false)
+
+        myView.innerClassLessonImage3.setOnClickListener {
+            val urlIntent = Intent(context, TryKotlinActivity::class.java)
+            urlIntent.putExtra("KotlinUrl", url)
+            startActivity(urlIntent)
+        }
+
+        return myView
     }
 
 
--- app/src/main/java/com/quentin/is3261/pepeteacheskotlin/ClassObjectNestedClassPart1.kt
+++ app/src/main/java/com/quentin/is3261/pepeteacheskotlin/ClassObjectNestedClassPart1.kt
@@ -1,11 +1,14 @@
 package com.quentin.is3261.pepeteacheskotlin
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_basic_types_ranges_lesson_part2.view.*
+import kotlinx.android.synthetic.main.fragment_class_object_nested_class_part1.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,7 +22,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class ClassObjectNestedClassPart1 : Fragment() {
 
-    private val url = "https://www.tutorialspoint.com/tpcg.php?p=TdGTLL"
+    private val url = "https://pl.kotl.in/B1FkYTe6m"
 
     companion object {
         fun newInstance() = ClassObjectNestedClassPart1()
@@ -27,7 +30,15 @@ class ClassObjectNestedClassPart1 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_class_object_nested_class_part1, container, false)
+        val myView = inflater.inflate(R.layout.fragment_class_object_nested_class_part1, container, false)
+
+        myView.nestedClassLessonImage1.setOnClickListener {
+            val urlIntent = Intent(context, TryKotlinActivity::class.java)
+            urlIntent.putExtra("KotlinUrl", url)
+            startActivity(urlIntent)
+        }
+
+        return myView
     }
 
 
